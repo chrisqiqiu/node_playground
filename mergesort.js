185b741794376b69a349fc081fffe5256fa72baf
Chris Qiu <churchqq030@hotmail.com>
2018-07-18 22:36:34 +1000

    merge sort

--- mergesort.js
+++ mergesort.js
@@ -4,23 +4,21 @@
 
 function merge(nums,l,m,r){
 
-    n1 = m -l +1
-    n2 = r -m 
+    let n1 = m -l +1
+    let n2 = r -m 
 
-    firstSubarray = []
-    secondSubarray = []
     // Copy data to temp arrays
-    for(let i = 0 ; i < n1; i ++){
-        firstSubarray.push(nums[l+i])
-    }
+    const firstSubarray = nums.slice(0,m)
+    console.log("firstSubarray")
+    console.log(firstSubarray)
 
-    for(let j = 0 ; j < n2; j ++){
-        secondSubarray.push(nums[m+1+j])
-    }
+    const secondSubarray = nums.slice(m,r)
+    console.log("secondSubarray")
+    console.log(secondSubarray)
 
-    i = 0
-    j = 0
-    k = l
+    let i = 0
+    let j = 0
+    let k = l
 
     while (i < n1 && j < n2){
         if (firstSubarray[i] <= secondSubarray[j]){
@@ -29,41 +27,81 @@ function merge(nums,l,m,r){
             i++
         }
         else{
-            nums[k]=secondSubarray[j]
+            nums[k] = secondSubarray[j]
             j++
         }
         k++
     }
 
-    // Copy the remaining elements of L[], if there
-    // are any
+    // Copy the remaining elements of L[], if there are any
     while (i < n1){
         nums[k] = firstSubarray[i]
         i ++
         k ++
     }
 
-    // Copy the remaining elements of R[], if there
-    // are any
+    // Copy the remaining elements of R[], if there are any
     while (j < n2){
-        nums[k] = secondSubarrayj[j]
-        j += 1
-        k += 1
+        nums[k] = secondSubarray[j]
+        j ++
+        k ++
     }
         
-    //console.log(firstSubarray)
+    
 }
 
-function mergeSort(nums,l,r) {
-    if (l<r){
-        m = (l+(r-1))/2
-        
-        mergeSort(nums,l,m)
-        mergeSort(nums,m+1,r)
-        merge(nums,l,m,r)
-        //console.log(nums)
-    }
+function mergeSort(nums) {
     
+    if (nums.length < 2 ) {
+        return nums;
+    }
+
+    const length = nums.length;
+    const middle = Math.floor(length/2);
+    const left = nums.slice(0,middle);
+    const right = nums.slice(middle,length);
+    
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return stitch(sortedLeft,sortedRight);
+
+    // if (l<r){
+    //     m = Math.floor((l+r)/2)
+    //     console.log(`l:${l} m:${m} r:${r}`)
+    //     mergeSort(nums,l,m)
+    //     mergeSort(nums,m+1,r)
+    //     merge(nums,l,m,r)
+    //     //console.log(nums)
+    // }
+    
+
+}
+
+
+
+const stitch = (left, right) =>{
+    const results =[];
+
+    while (left.length && right.length) {
+        if(left[0] <=right[0]){
+            results.push(left.shift());
+        }
+        else{
+            results.push(right.shift());
+        }
+    }
+
+    // while(left.length){
+    //     results.push(left.shift());
+    // }
+
+    // while(right.length){
+    //     results.push(right.shift());
+    // }
+
+    return [...results, ...left, ...right];
+
 
 }
 
@@ -71,5 +109,5 @@ function mergeSort(nums,l,r) {
 
 var nums = [10,5,3,8,2,6,4,7,9,1]
 
-mergeSort(nums,0,nums.length)
-console.log(nums)
+mergeSort(nums)
+console.log(mergeSort(nums))
